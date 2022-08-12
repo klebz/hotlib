999b682628efd3a2d86dfe9f73d2825b67d70f98
klebs <none>
2022-08-12 14:32:19 -0400

    take lifetime away from Build struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-//! For watching and loading a Rust library in Rust.
+//! For watching and loading a Rust library in
+//! Rust.
 //!
-//! You are likely looking for the [watch function docs](./fn.watch.html).
+//! You are likely looking for the [watch function
+//! docs](./fn.watch.html).
 
 use notify::Watcher as NotifyWatcher;
 use notify::EventHandler;
@@ -12,44 +14,53 @@ use thiserror::Error;
 #[doc(inline)]
 pub use libloading::{self, Library, Symbol};
 
-/// Watches and re-builds the library upon changes to its source code.
+/// Watches and re-builds the library upon changes
+/// to its source code.
 pub struct Watch {
     package_info: PackageInfo,
-    _watcher: notify::RecommendedWatcher,
-    event_rx: crossbeam_channel::Receiver<Result<notify::Event,notify::Error>>,
+    _watcher:     notify::RecommendedWatcher,
+    event_rx:     crossbeam_channel::Receiver<Result<notify::Event,notify::Error>>,
 }
 
 struct PackageInfo {
-    manifest_path: PathBuf,
-    src_path: PathBuf,
-    lib_name: String,
+    manifest_path:   PathBuf,
+    src_path:        PathBuf,
+    lib_name:        String,
     target_dir_path: PathBuf,
 }
 
-/// The information required to build the package's dylib target.
+/// The information required to build the
+/// package's dylib target.
 pub struct Package<'a> {
     info: &'a PackageInfo,
 }
 
-/// The result of building a package's dynamic library.
+/// The result of building a package's dynamic
+/// library.
 ///
-/// This can be used to load the dynamic library either in place or via a temporary file as to allow
-/// for re-building the package while using the library.
-pub struct Build<'a> {
-    lib_name: &'a str,
-    target_dir_path: &'a Path,
-    timestamp: SystemTime,
-    output: std::process::Output,
+/// This can be used to load the dynamic library
+/// either in place or via a temporary file as to
+/// allow for re-building the package while using
+/// the library.
+pub struct Build {
+    lib_name:        String,
+    target_dir_path: PathBuf,
+    timestamp:       SystemTime,
+    output:          std::process::Output,
 }
 
-/// A wrapper around a `libloading::Library` that cleans up the library on `Drop`.
+/// A wrapper around a `libloading::Library` that
+/// cleans up the library on `Drop`.
 pub struct TempLibrary {
     build_timestamp: SystemTime,
-    path: PathBuf,
-    // This is always `Some`. An `Option` is only used so that the library may be `Drop`ped during
-    // the `TempLibrary`'s `drop` implementation before the temporary library file at `path` is
-    // removed.
-    lib: Option<libloading::Library>,
+    path:            PathBuf,
+
+    // This is always `Some`. An `Option` is only
+    // used so that the library may be `Drop`ped
+    // during the `TempLibrary`'s `drop`
+    // implementation before the temporary library
+    // file at `path` is removed.
+    lib:             Option<libloading::Library>,
 }
 
 /// Errors that might occur within the `watch` function.
@@ -81,7 +92,8 @@ pub enum WatchError {
     },
 }
 
-/// Errors that might occur while building a library instance.
+/// Errors that might occur while building
+/// a library instance.
 #[derive(Debug, Error)]
 pub enum BuildError {
     #[error("an IO error occurred while attempting to invoke cargo: {err}")]
@@ -96,7 +108,8 @@ pub enum BuildError {
     },
 }
 
-/// A process' output indicates unsuccessful completion.
+/// A process' output indicates unsuccessful
+/// completion.
 #[derive(Debug, Error)]
 #[error("cargo process exited unsuccessfully with status code: {code:?}: {stderr}")]
 pub struct ExitStatusUnsuccessfulError {
@@ -104,7 +117,8 @@ pub struct ExitStatusUnsuccessfulError {
     pub stderr: String,
 }
 
-/// Errors that might occur while waiting for the next library instance.
+/// Errors that might occur while waiting for the
+/// next library instance.
 #[derive(Debug, Error)]
 pub enum NextError {
     #[error("the channel used to receive file system events was closed")]
@@ -116,7 +130,8 @@ pub enum NextError {
     },
 }
 
-/// Errors that might occur while loading a built library.
+/// Errors that might occur while loading a built
+/// library.
 #[derive(Debug, Error)]
 pub enum LoadError {
     #[error("an IO error occurred: {err}")]
@@ -146,20 +161,28 @@ impl ExitStatusUnsuccessfulError {
 
 /// Watch the library at the given `Path`.
 ///
-/// The given `Path` should point to the `Cargo.toml` of the package used to build the library.
+/// The given `Path` should point to the
+/// `Cargo.toml` of the package used to build the
+/// library.
 ///
-/// When a library is being "watched", the library will be re-built any time some filesystem event
-/// occurs within the library's source directory. The target used is the first "dylib" discovered
-/// within the package.
+/// When a library is being "watched", the library
+/// will be re-built any time some filesystem
+/// event occurs within the library's source
+/// directory. The target used is the first
+/// "dylib" discovered within the package.
 ///
-/// The `notify` crate is used to watch for file-system events in a cross-platform manner.
+/// The `notify` crate is used to watch for
+/// file-system events in a cross-platform manner.
 pub fn watch(path: &Path) -> Result<Watch, WatchError> {
+
     if !path.ends_with("Cargo.toml") && !path.ends_with("cargo.toml") {
         return Err(WatchError::InvalidPath);
     }
 
-    // Run the `cargo metadata` command to retrieve JSON containing lib target info.
+    // Run the `cargo metadata` command to
+    // retrieve JSON containing lib target info.
     let manifest_path_str = format!("{}", path.display());
+
     let output = std::process::Command::new("cargo")
         .arg("metadata")
         .arg("--manifest-path")
@@ -267,30 +290,35 @@ impl EventHandler for ChannelSender {
 }
 
 impl Watch {
+
     /// The path to the package's `Cargo.toml`.
     pub fn manifest_path(&self) -> &Path {
         &self.package_info.manifest_path
     }
 
-    /// The path to the source directory being watched.
+    /// The path to the source directory being
+    /// watched.
     pub fn src_path(&self) -> &Path {
         &self.package_info.src_path
     }
 
-    /// Wait for the library to be re-built after some change.
+    /// Wait for the library to be re-built after
+    /// some change.
     pub fn next(&self) -> Result<Package, NextError> {
         loop {
             let event = match self.event_rx.recv() {
                 Err(_) => return Err(NextError::ChannelClosed),
                 Ok(event) => event,
             };
+
             if check_raw_event(event?)? {
                 return Ok(self.package());
             }
         }
     }
 
-    /// The same as `next`, but returns early if there are no pending events.
+    /// The same as `next`, but returns early if
+    /// there are no pending events.
     pub fn try_next(&self) -> Result<Option<Package>, NextError> {
         for event in self.event_rx.try_iter() {
             if check_raw_event(event?)? {
@@ -300,9 +328,12 @@ impl Watch {
         Ok(None)
     }
 
-    /// Manually retrieve the library's package immediately without checking for file events.
+    /// Manually retrieve the library's package
+    /// immediately without checking for file
+    /// events.
     ///
-    /// This is useful for triggering an initial build during model initialisation.
+    /// This is useful for triggering an initial
+    /// build during model initialisation.
     pub fn package(&self) -> Package {
         let info = &self.package_info;
         Package { info }
@@ -310,6 +341,7 @@ impl Watch {
 }
 
 impl<'a> Package<'a> {
+
     /// The path to the package's `Cargo.toml`.
     pub fn manifest_path(&self) -> &Path {
         &self.info.manifest_path
@@ -321,7 +353,7 @@ impl<'a> Package<'a> {
     }
 
     /// Builds the package's dynamic library target.
-    pub fn build(&self) -> Result<Build<'a>, BuildError> {
+    pub fn build(&self) -> Result<Build, BuildError> {
         let PackageInfo {
             ref manifest_path,
             ref lib_name,
@@ -350,13 +382,14 @@ impl<'a> Package<'a> {
         Ok(Build {
             timestamp,
             output,
-            lib_name,
-            target_dir_path,
+            lib_name:        lib_name.to_string(),
+            target_dir_path: target_dir_path.to_path_buf(),
         })
     }
 }
 
-impl<'a> Build<'a> {
+impl Build {
+
     /// The output of the cargo process.
     pub fn cargo_output(&self) -> &std::process::Output {
         &self.output
@@ -376,25 +409,33 @@ impl<'a> Build<'a> {
             .with_extension(dylib_ext())
     }
 
-    /// The path to the temporary dynamic library clone that will be created upon `load`.
+    /// The path to the temporary dynamic library
+    /// clone that will be created upon `load`.
     pub fn tmp_dylib_path(&self) -> PathBuf {
         tmp_dir()
             .join(self.tmp_file_stem())
             .with_extension(dylib_ext())
     }
 
-    /// Copy the library to the platform's temporary directory and load it from there.
+    /// Copy the library to the platform's
+    /// temporary directory and load it from
+    /// there.
     ///
-    /// Note that the copied dynamic library will be removed on `Drop`.
+    /// Note that the copied dynamic library will
+    /// be removed on `Drop`.
     pub fn load(self) -> Result<TempLibrary, LoadError> {
+
         let dylib_path = self.dylib_path();
-        let tmp_path = self.tmp_dylib_path();
-        let tmp_dir = tmp_path.parent().expect("temp dylib path has no parent");
+        let tmp_path   = self.tmp_dylib_path();
+        let tmp_dir    = tmp_path.parent().expect("temp dylib path has no parent");
 
         // If the library already exists, load it.
         loop {
+
             if tmp_path.exists() {
-                // This is some voodoo to enable reloading of dylib on mac os
+
+                // This is some voodoo to enable
+                // reloading of dylib on mac os
                 if cfg!(target_os = "macos") {
                     std::process::Command::new("install_name_tool")
                         .current_dir(tmp_dir)
@@ -421,16 +462,19 @@ impl<'a> Build<'a> {
                 };
                 return Ok(tmp);
             }
+
             // Copy the dylib to the tmp location.
             std::fs::create_dir_all(tmp_dir).map_err(|err| LoadError::Io { err })?;
             std::fs::copy(&dylib_path, &tmp_path).map_err(|err| LoadError::Io { err })?;
         }
     }
 
-    /// Load the library from it's existing location.
+    /// Load the library from it's existing
+    /// location.
     ///
-    /// Note that if you do this, you will have to ensure the returned `Library` is dropped before
-    /// attempting to re-build the library.
+    /// Note that if you do this, you will have to
+    /// ensure the returned `Library` is dropped
+    /// before attempting to re-build the library.
     pub fn load_in_place(self) -> Result<libloading::Library, libloading::Error> {
         let dylib_path = self.dylib_path();
         libloading::Library::new(dylib_path)
@@ -438,20 +482,27 @@ impl<'a> Build<'a> {
 
     // The file stem of the built dynamic library.
     fn file_stem(&self) -> String {
-        // TODO: On windows, the generated lib does not contain the "lib" prefix.
-        // A proper solution would likely involve retrieving the file stem from cargo itself.
+
+        // TODO: On windows, the generated lib
+        // does not contain the "lib" prefix.
+        //
+        // A proper solution would likely involve
+        // retrieving the file stem from cargo
+        // itself.
         #[cfg(target_os = "windows")]
         {
             format!("{}", self.lib_name)
         }
+
         #[cfg(not(target_os = "windows"))]
         {
             format!("lib{}", self.lib_name)
         }
     }
 
-    // Produce the file stem for the temporary dynamic library clone that will be created upon
-    // `load`.
+    // Produce the file stem for the temporary
+    // dynamic library clone that will be created
+    // upon `load`.
     fn tmp_file_stem(&self) -> String {
         let timestamp_slug = slugify(format!("{}", humantime::format_rfc3339(self.timestamp)));
         format!("{}-{}", self.file_stem(), timestamp_slug)
@@ -459,21 +510,25 @@ impl<'a> Build<'a> {
 }
 
 impl TempLibrary {
+
     /// The inner `libloading::Library`.
     ///
-    /// This may also be accessed via the `Deref` implementation.
+    /// This may also be accessed via the `Deref`
+    /// implementation.
     pub fn lib(&self) -> &libloading::Library {
         self.lib
             .as_ref()
             .expect("lib should always be `Some` until `Drop`")
     }
 
-    /// The time at which the original library was built.
+    /// The time at which the original library was
+    /// built.
     pub fn build_timestamp(&self) -> SystemTime {
         self.build_timestamp
     }
 
-    /// The path at which the loaded temporary library is located.
+    /// The path at which the loaded temporary
+    /// library is located.
     pub fn path(&self) -> &Path {
         &self.path
     }
@@ -498,12 +553,14 @@ fn tmp_dir() -> PathBuf {
     std::env::temp_dir().join("hotlib")
 }
 
-// Whether or not the given event should trigger a rebuild.
+// Whether or not the given event should trigger
+// a rebuild.
 fn _check_event(_event: notify::Event) -> bool {
     true
 }
 
-// Whether or not the given event should trigger a rebuild.
+// Whether or not the given event should trigger
+// a rebuild.
 fn check_raw_event(event: notify::Event) -> Result<bool, NextError> {
 
     use notify::event::*;
@@ -527,18 +584,22 @@ fn check_raw_event(event: notify::Event) -> Result<bool, NextError> {
 //
 // TODO: This should be exposed from cargo.
 fn dylib_ext() -> &'static str {
+
     #[cfg(target_os = "linux")]
     {
         return "so";
     }
+
     #[cfg(any(target_os = "macos", target_os = "ios"))]
     {
         return "dylib";
     }
+
     #[cfg(target_os = "windows")]
     {
         return "dll";
     }
+
     #[cfg(not(any(
         target_os = "linux",
         target_os = "macos",
